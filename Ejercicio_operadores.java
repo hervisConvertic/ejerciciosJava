b66e53a65556fe8ff9de1080945e15531429197f
hervisConvertic <hervis.cifuentes@convertic.com.co>
2022-10-26 10:01:34 -0500

    nuevo hola mundo de prueba

--- Ejercicio_operadores.java
+++ Ejercicio_operadores.java
@@ -2,5 +2,6 @@ public class Ejercicio_operadores {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hola mundo");
+        System.out.println("hola mundo nuevo");
     }
 }
