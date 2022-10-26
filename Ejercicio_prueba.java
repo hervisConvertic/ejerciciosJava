00aa7cdf1e79117ef6232a0614ce57495c1efb8d
hervisConvertic <hervis.cifuentes@convertic.com.co>
2022-10-26 09:50:36 -0500

    se eliminarn ejercicios de prueba

--- Ejercicio_prueba.java
+++
@@ -1,6 +0,0 @@
-public class Ejercicio_prueba {
-    public static void main(String[] args) {
-        System.out.println("hellow word");
-        System.out.println("holla hervis");
-    }
-}
--- Ejercicio_pruebaDos.java
+++
@@ -1,5 +0,0 @@
-public class Ejercicio_pruebaDos {
-    public static void main(String[] args) {
-        System.out.println("estoy pero ahora quiero hacer una modificacion");
-    }
-}
