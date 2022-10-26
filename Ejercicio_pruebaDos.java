1f5e527de780572388a0967fe766959ec1512a35
hervisConvertic <hervis.cifuentes@convertic.com.co>
2022-10-26 09:48:50 -0500

    estoy volviendo a probar

--- Ejercicio_pruebaDos.java
+++ Ejercicio_pruebaDos.java
@@ -1,5 +1,5 @@
 public class Ejercicio_pruebaDos {
     public static void main(String[] args) {
-        System.out.println("estoy volviendo a probar como subir desde intellij");
+        System.out.println("estoy pero ahora quiero hacer una modificacion");
     }
 }
